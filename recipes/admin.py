347f8d1906fc69e6ee3a51dcb87dd341b7c7b880
Suk Chung <sukchung7@gmail.com>
2022-11-12 15:32:38 -0800

    Finished scrumptious app

---
+++ recipes/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from recipes.models import Recipe, RecipeStep, Ingredient
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "id",
+    )
+
+
+@admin.register(RecipeStep)
+class RecipeStepAdmin(admin.ModelAdmin):
+    list_display = (
+        "step_number",
+        "instruction",
+        "id",
+    )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "amount",
+        "food_item",
+        "id",
+    )
---
+++ recipes/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from recipes.views import (
+    recipe_list,
+    show_recipe,
+    create_recipe,
+    edit_recipe,
+    my_recipe_list,
+)
+
+
+urlpatterns = [
+    path("", recipe_list, name="main_page"),
+    path("create/", create_recipe, name="create_recipe"),
+    path("<int:id>/edit/", edit_recipe, name="edit_recipe"),
+    path("mine/", my_recipe_list, name="my_recipe_list"),
+    path("<int:id>/", show_recipe, name="show_recipe"),
+]
